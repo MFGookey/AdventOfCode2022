ac653aa87b342661ca2a842f409e311ce3c967a8
MFGookey <MFGookey@users.noreply.github.com>
2022-12-05 19:09:30 -0500

    Day 4 Part 1

---
+++ 2022-12-04/SantasRangers/SantasRangers.Cmd/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Common.Utilities.IO;
+using SantasRangers.Core;
+using System.Linq;
+
+namespace SantasRangers.Cmd
+{
+  /// <summary>
+  /// The entrypoint for SantasRangers.Cmd
+  /// </summary>
+  public class Program
+  {
+    /// <summary>
+    /// SantasRangers.Cmd entry point
+    /// </summary>
+    /// <param name="args">Command line arguments (not used)</param>
+    static void Main(string[] args)
+    {
+      var filePath = "./input";
+      var reader = new FileReader();
+
+      var ranges = reader.ReadFile(filePath).Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+      var count = ranges.Where(r => RangeCalculator.RangesOverlap(r)).Count();
+
+      Console.WriteLine(count);
+      _ = Console.ReadLine();
+    }
+  }
+}
---
+++ 2022-12-04/SantasRangers/SantasRangers.Core/RangeCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SantasRangers.Core
+{
+  public static class RangeCalculator
+  {
+    public static bool RangesOverlap(string ranges)
+    {
+      var pair = ranges.Split(',', StringSplitOptions.RemoveEmptyEntries).ToArray();
+      
+      if (pair.Length != 2)
+      {
+        throw new ArgumentException($"Range {ranges} must be two comma separated values");
+      }
+
+      var first = SplitRange(pair[0]);
+      var second = SplitRange(pair[1]);
+
+      return (first.Item1 >= second.Item1 && first.Item2 <= second.Item2) || (second.Item1 >= first.Item1 && second.Item2 <= first.Item2);
+    }
+
+    private static Tuple<int, int> SplitRange(string range)
+    {
+      var values = range.Split('-', StringSplitOptions.RemoveEmptyEntries).Where(v => int.TryParse(v, out _)).Select(v => int.Parse(v)).ToArray();
+
+      if (values.Count() != 2)
+      {
+        throw new ArgumentException($"Range {range} must be two integers separated by a \"-\"");
+      }
+
+      return Tuple.Create(values[0], values[1]);
+    }
+  }
+}
